ca9f8ef44390799a3cc6edb8043a66aa2513fd12
Raoul <Raoul@10.129.10.228>
2014-05-08 13:37:42 +0200

    View.php geeft niet langer foutmeldingen als het bestand niet kan worden geopend

--- core/view.php
+++ core/view.php
@@ -1,53 +1,49 @@
 <?php
 
-class view
-{
-    private $output = '';
-    
-    function __construct()
-    {
-	
-    }
-    
-    public function load($file, $template = '')
-    {
-        if(!empty($template))
-        {
-            ob_start();
-            include("view/".$file.".html");
-            $content = ob_get_contents();
-            ob_end_clean();
-            $this->output = str_replace('{'.  strtoupper($template).'}',$content,$this->output);
+class view {
+
+        private $output = '';
+
+        function __construct() {
+                
         }
-        else
-        {
-            $this->output .= file_get_contents("view/".$file.".html");
+
+        public function load($file, $template = '') {
+                if (!empty($template)) {
+                        if (file_exists("view/" . $file . ".html")) {
+                                ob_start();
+                                include("view/" . $file . ".html");
+                                $content = ob_get_contents();
+                                ob_end_clean();
+                                $this->output = str_replace('{' . strtoupper($template) . '}', $content, $this->output);
+                        }
+                } else {
+                        if (file_exists("view/" . $file . ".html")) {
+                                $this->output .= file_get_contents("view/" . $file . ".html");
+                        }
+                }
         }
-            
-    }
-    
-    public function replace($template, $value)
-    {
-        if(!is_array($value)) $this->output = str_replace('{'.strtoupper($template).'}', $value, $this->output);
-    }
-	
-	public function replace_array($array)
-	{
-		foreach($array as $key=>$value)
-		{
-			if(!is_array($value)) $this->replace($key, $value);
-		}
-	}
-	
-	public function cleanup($template)
-	{
-		$this->output = preg_replace('/\{'.$template.'(\w*)\}/', '', $this->output);
-	}
-    
-    public function __destruct()
-    {
-        echo $this->output;
-    }
+
+        public function replace($template, $value) {
+                if (!is_array($value))
+                        $this->output = str_replace('{' . strtoupper($template) . '}', $value, $this->output);
+        }
+
+        public function replace_array($array) {
+                foreach ($array as $key => $value) {
+                        if (!is_array($value))
+                                $this->replace($key, $value);
+                }
+        }
+
+        public function cleanup($template) {
+                $this->output = preg_replace('/\{' . $template . '(\w*)\}/', '', $this->output);
+        }
+
+        public function __destruct() {
+                echo $this->output;
+        }
+
 }
 
 ?>
