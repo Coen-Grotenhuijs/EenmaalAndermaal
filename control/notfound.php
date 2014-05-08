213326846ce3881a21af5041edf285183db4df1e
Raoul <Raoul@10.129.10.228>
2014-05-08 13:41:49 +0200

    notfound geupdate

--- control/notfound.php
+++ control/notfound.php
@@ -5,6 +5,7 @@ class notfoundControl extends control
 	public function run()
 	{
 		$this->loadView('notfound','content');
+                $this->replaceView('title','Pagina niet gevonden!');
 	}
 }
 
