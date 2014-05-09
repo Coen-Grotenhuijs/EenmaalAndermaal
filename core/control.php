aba9d3d97cc1b9b41a0f889f593560f7f458a962
Raoul <Raoul@10.129.10.228>
2014-05-09 11:24:20 +0200

    Dashboard model basis functionaliteit gegeven.

--- core/control.php
+++ core/control.php
@@ -2,127 +2,111 @@
 
 include("core/session.php");
 
-class control extends session
-{
-	protected $view;
-	
-	protected $get = array();
-	public $post = array();
+class control extends session {
 
-    public function __construct()
-    {
-		// sessie model laden
-		$this->loadModel("session");
-		
-		// model dat hoort bij de huidige control laden
-		$classname = get_class($this);
-		$this->loadModel(str_replace("Control","",$classname));
-		
-		
-		// view aanmaken
-		$this->view = new view();
-        
-		// HTML template laden
-		if($classname!='ajaxControl') $this->loadView("template");
-		
-		// huidige URL bepalen
-		$urltemp = explode('/',$_SERVER["REQUEST_URI"]);
-		$urltemp2 = explode('?',$urltemp[count($urltemp)-1]);
-		$url = $urltemp2[0];
-		
-		// request data filteren
-		$this->filterrequestdata();
-		
-		// sessie constructor uitvoeren
-		parent::__construct();
-		
-		// Ingelogd
-		if($this->logged_in)
-		{
-			$this->loadView('topbar/loggedin', 'topbar');
-		}
-		else
-		{
-			$this->loadView('topbar/loggedout', 'topbar');
-		}
+        protected $view;
+        protected $get = array();
+        public $post = array();
 
-		// Inlog formulier errors weergeven
-		if(!empty($this->loginform))
-		{
-			$this->view->replace_array($this->loginform->geterrors());
-			$this->view->replace_array($this->loginform->getclasses());
-		}
-	}
-	
-	public function filterrequestdata()
-	{
-		foreach($_GET as $key=>$value)
-		{
-			$this->get[$key] = $this->filter($value);
-		}
-		foreach($_POST as $key=>$value)
-		{
-			$this->post[$key] = $this->filter($value);
-		}
-	}
-	
-	private function filter($data)
-	{
-		return str_replace("'","",$data);
-	}
-    
-	public function loadModel($model)
-	{
-		if(file_exists("model/".$model.".php"))
-		{
-			include("model/".$model.".php");
-			$classname = $model."Model";
-			if(class_exists($classname)) $this->$classname = new $classname;
-		}
-	}
-	
-	public function loadView($file, $template = '')
-	{
-		$this->view->load($file, $template);
-	}
-	
-	public function replaceView($template, $value)
-	{
-		$this->view->replace($template, $value);
-	}
-	
-	private function formfill()
-	{
-		foreach($_POST as $key=>$value)
-		{
-			$this->replaceView('POST_'.$key,$value);
-			if(is_int($value)) $this->replaceView('POST_'.$key.'_'.$value,'selected');
-		}
-		foreach($_GET as $key=>$value)
-		{
-			$this->replaceView('GET_'.$key,$value);
-		}
-	}
-	
-	private function sessionfill()
-	{
-		foreach($_SESSION as $key=>$value)
-		{
-			$this->replaceView('SESSION_'.$key,$value);
-		}
-	}
+        public function __construct() {
+                // sessie model laden
+                $this->loadModel("session");
 
-	public function __destruct()
-	{
-		$this->formfill();
-		$this->sessionfill();
-		$this->view->cleanup('POST');
-		$this->view->cleanup('GET');
-		$this->view->cleanup('ERROR');
-		$this->view->cleanup('CLASS');
-		$this->view->cleanup('JS');
-		unset($this->view);
-	}
+                // model dat hoort bij de huidige control laden
+                $classname = get_class($this);
+                $this->loadModel(str_replace("Control", "", $classname));
+
+
+                // view aanmaken
+                $this->view = new view();
+
+                // HTML template laden
+                if ($classname != 'ajaxControl')
+                        $this->loadView("template");
+
+                // huidige URL bepalen
+                $urltemp = explode('/', $_SERVER["REQUEST_URI"]);
+                $urltemp2 = explode('?', $urltemp[count($urltemp) - 1]);
+                $url = $urltemp2[0];
+
+                // request data filteren
+                $this->filterrequestdata();
+
+                // sessie constructor uitvoeren
+                parent::__construct();
+
+                // Ingelogd
+                if ($this->logged_in) {
+                        $this->loadView('topbar/loggedin', 'topbar');
+                } else {
+                        $this->loadView('topbar/loggedout', 'topbar');
+                }
+
+                // Inlog formulier errors weergeven
+                if (!empty($this->loginform)) {
+                        $this->view->replace_array($this->loginform->geterrors());
+                        $this->view->replace_array($this->loginform->getclasses());
+                }
+        }
+
+        public function filterrequestdata() {
+                foreach ($_GET as $key => $value) {
+                        $this->get[$key] = $this->filter($value);
+                }
+                foreach ($_POST as $key => $value) {
+                        $this->post[$key] = $this->filter($value);
+                }
+        }
+
+        private function filter($data) {
+                return str_replace("'", "", $data);
+        }
+
+        public function loadModel($model) {
+                if (file_exists("model/" . $model . ".php")) {
+                        include("model/" . $model . ".php");
+                        $classname = $model . "Model";
+                        if (class_exists($classname))
+                                $this->$classname = new $classname;
+                }
+        }
+
+        public function loadView($file, $template = '') {
+                $this->view->load($file, $template);
+        }
+
+        public function replaceView($template, $value) {
+                $this->view->replace($template, $value);
+        }
+
+        private function formfill() {
+                foreach ($_POST as $key => $value) {
+                        $this->replaceView('POST_' . $key, $value);
+                        if (is_int($value))
+                                $this->replaceView('POST_' . $key . '_' . $value, 'selected');
+                }
+                foreach ($_GET as $key => $value) {
+                        $this->replaceView('GET_' . $key, $value);
+                }
+        }
+
+        private function sessionfill() {
+                foreach ($_SESSION as $key => $value) {
+                        $this->replaceView('SESSION_' . $key, $value);
+                }
+        }
+
+        public function __destruct() {
+                $this->formfill();
+                $this->sessionfill();
+                $this->view->cleanup('POST');
+                $this->view->cleanup('GET');
+                $this->view->cleanup('ERROR');
+                $this->view->cleanup('CLASS');
+                $this->view->cleanup('JS');
+                unset($this->view);
+        }
+
 }
 
 ?>
--- model/dashboard.php
+++ model/dashboard.php
@@ -4,27 +4,31 @@ class dashboardModel extends model
 {
         public function getBiedingen()
         {
-                
+                $data = $this->db->fetchQueryAll("SELECT TOP 3 FROM Voorwerp INNER JOIN Bod ON Voorwerp.Voorwerpnummer = Bod.Voorwerpnummer WHERE Bod.Gebruiker = '".$this->getCurrentUser()."'");
+                return $data;
         }
         
         public function getVoorwerpen()
         {
-                
+                $data = $this->db->fetchQueryAll("SELECT TOP 3 FROM Voorwerp WHERE Verkoper = '".$this->getCurrentUser()."'");
+                return $data;
         }
         
         public function getSuggesties()
         {
-                
+                $data = $this->db->fetchQueryAll("SELECT TOP 5 FROM Voorwerp");
         }
         
         public function getProfielgegevens()
         {
-                
+                $data = $this->db->fetchQuery("SELECT * FROM Gebruiker WHERE Gebruikersnaam = '".$this->getCurrentUser()."'");
+                return $data;
         }
         
         public function getScore()
         {
-                
+                $data = $this->db->fetchQueryAll("SELECT Feedbacksoort, SUM(*) FROM Feedback WHERE Gebruikersnaam = '".$this->getCurrentUser()."' GROUP BY Feedbacksoort");
+                return $data;
         }
 }
 
