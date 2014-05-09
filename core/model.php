86a4c38ae16233561f1c4418043a55bef77d5839
Raoul <Raoul@10.129.10.228>
2014-05-09 11:04:24 +0200

    Session model in model basis gestopt zodat die functies overal toegankelijk zijn.

--- core/model.php
+++ core/model.php
@@ -14,6 +14,22 @@ class model
 		return md5($string);
 	}
 
+	public function getUser($user)
+	{
+		return $this->db->fetchquery("SELECT * FROM Gebruiker WHERE Gebruikersnaam = '".$user."'");
+	}
+	
+	public function getUserPass($user, $pass)
+	{
+		return $this->db->fetchquery("SELECT * FROM Gebruiker WHERE Gebruikersnaam = '".$user."' AND Wachtwoord = '".$pass."'");
+	}
+
+	public function validateSession($data)
+	{
+		$result = $this->db->fetchquery("SELECT * FROM Gebruiker WHERE Gebruikersnaam = '".$data['user']."' AND Wachtwoord = '".$data['pass']."'");
+		if(!empty($result)) return true;
+		return false;
+	}        
 }
 
 ?>
---
+++ model/dashboard.php
@@ -0,0 +1,31 @@
+<?php
+
+class dashboardModel extends model
+{
+        public function getBiedingen()
+        {
+                
+        }
+        
+        public function getVoorwerpen()
+        {
+                
+        }
+        
+        public function getSuggesties()
+        {
+                
+        }
+        
+        public function getProfielgegevens()
+        {
+                
+        }
+        
+        public function getScore()
+        {
+                
+        }
+}
+
+?>
